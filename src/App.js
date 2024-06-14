65eefd6eb130ca011fdf44f9bf53cb0823e182c5
unknown <shruti300000@gmail.com>
2024-06-14 21:25:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -8,19 +8,25 @@ import Login from './Pages/Login';
 import NewsPaper from './Pages/Newspaper'
 import Magazine from './Pages/Magazine'
 import Publication from './Pages/Publication'
+import Cart from './Pages/Cart';
+import Cart2 from './Pages/Cart2';
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/newspaper" element={<NewsPaper />} />
-        <Route path="/magazine" element={<Magazine />} />
-        <Route path="/publication" element={<Publication />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/login" element={<Login />} />
+    //     <Route path="/newspaper" element={<NewsPaper />} />
+    //     <Route path="/magazine" element={<Magazine />} />
+    //     <Route path="/publication" element={<Publication />} />
+    //   </Routes>
+    // </Router>
+
+    <>
+    <Cart/>
+    </>
   
   );
 }
